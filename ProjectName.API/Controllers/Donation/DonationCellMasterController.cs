53133d2cd1432390d0a83a26449a94a17b55e1bd
Muhammad Ahsan <53137713+ahsansoftengineer@users.noreply.github.com>
2023-05-21 18:18:03 +0500

    DCO AND DONATION SUB TYPE CONTROLLER WORKING

--- ProjectName.API/Controllers/Donation/DonationCellMasterController.cs
+++ ProjectName.API/Controllers/Donation/DonationCellMasterController.cs
@@ -1,11 +1,107 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using ProjectName.API.Controllers.Base;
+using ProjectName.Domain.Base;
+using ProjectName.Domain.Common;
+using ProjectName.Domain.DTOs.Donation;
+using ProjectName.Infra.Entity.Donationz;
+using ProjectName.Infra.Repo;
+using X.PagedList;
 
 namespace ProjectName.API.Controllers.Donation
 {
   [Route("api/[controller]")]
   [ApiController]
-  public class DonationCellMasterController : ControllerBase
+  public class DonationCellMasterController : BaseController<DonationCellMasterController>
   {
+    public DonationCellMasterController(
+      ILogger<DonationCellMasterController> logger,
+      IMapper mapper,
+      IUnitOfWork unitOfWork) : base(logger, mapper, unitOfWork)
+    { }
+
+    [HttpGet]
+    public async Task<IActionResult> Gets([FromQuery] PaginateRequestFilter<DonationCellMaster, DonationCellMasterDtoSearch?> filter)
+    {
+      try
+      {
+        var list = await UnitOfWork.DonationCellMasters.Gets(filter);
+        var result = Mapper.Map<IPagedList<DonationCellMaster>, PaginateResponse<DonationCellMasterDto>>(list);
+        return Ok(result);
+      }
+      catch (Exception ex)
+      {
+        return CatchException(ex, nameof(Gets));
+      }
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> Get(int id)
+    {
+      var single = await UnitOfWork.DonationCellMasters.Get(
+        q => q.Id == id
+     //, new List<string> { "Org" }
+     );
+      var result = Mapper.Map<BaseDtoSingle<DonationCellMasterDto>>(single);
+      return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] DonationCellMasterDtoCreate data)
+    {
+      if (!ModelState.IsValid) return CreateInvalid();
+      try
+      {
+        var result = Mapper.Map<DonationCellMaster>(data);
+        await UnitOfWork.DonationCellMasters.Insert(result);
+        await UnitOfWork.Save();
+        return Ok(result);
+      }
+      catch (Exception ex)
+      {
+        return CatchException(ex, nameof(Create));
+      }
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<IActionResult> Update(int id, [FromBody] DonationCellMasterDtoCreate data)
+    {
+      if (!ModelState.IsValid || id < 1) return UpdateInvalid();
+      try
+      {
+        var item = await UnitOfWork.DonationCellMasters.Get(q => q.Id == id);
+
+        if (item == null) return UpdateNull();
+
+        var result = Mapper.Map(data, item);
+        UnitOfWork.DonationCellMasters.Update(item);
+        await UnitOfWork.Save();
+        return Ok(result);
+      }
+      catch (Exception ex)
+      {
+        return CatchException(ex, nameof(Update));
+      }
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+      if (id < 1) return DeleteInvalid();
+
+      var search = await UnitOfWork.DonationCellMasters.Get(q => q.Id == id);
+      if (search == null) return DeleteNull();
+
+      try
+      {
+        await UnitOfWork.DonationCellMasters.Delete(id);
+        await UnitOfWork.Save();
+      }
+      catch (Exception ex)
+      {
+        return CatchException(ex, nameof(Delete));
+      }
+      return NoContent();
+    }
   }
 }
--- ProjectName.API/Controllers/Donation/DonationSubTypeController.cs
+++ ProjectName.API/Controllers/Donation/DonationSubTypeController.cs
@@ -1,11 +1,108 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ProjectName.API.Controllers.Base;
+using ProjectName.Domain.Base;
+using ProjectName.Domain.Common;
+using ProjectName.Domain.DTOs.Donation;
+using ProjectName.Infra.Entity.Donationz;
+using ProjectName.Infra.Repo;
+using X.PagedList;
 
 namespace ProjectName.API.Controllers.Donation
 {
   [Route("api/[controller]")]
   [ApiController]
-  public class DonationSubTypeController : ControllerBase
+  public class DonationSubTypeController : BaseController<DonationSubTypeController>
   {
+    public DonationSubTypeController(
+      ILogger<DonationSubTypeController> logger,
+      IMapper mapper,
+      IUnitOfWork unitOfWork) : base(logger, mapper, unitOfWork)
+    { }
+
+    [HttpGet]
+    public async Task<IActionResult> Gets([FromQuery] PaginateRequestFilter<DonationSubType, DonationSubTypeDtoSearch?> filter)
+    {
+      try
+      {
+        var list = await UnitOfWork.DonationSubTypes.Gets(filter);
+        var result = Mapper.Map<IPagedList<DonationSubType>, PaginateResponse<DonationSubTypeDto>>(list);
+        return Ok(result);
+      }
+      catch (Exception ex)
+      {
+        return CatchException(ex, nameof(Gets));
+      }
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> Get(int id)
+    {
+      var single = await UnitOfWork.DonationSubTypes.Get(
+        q => q.Id == id
+     //, new List<string> { "Org" }
+     );
+      var result = Mapper.Map<BaseDtoSingle<DonationSubTypeDto>>(single);
+      return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] DonationSubTypeDtoCreate data)
+    {
+      if (!ModelState.IsValid) return CreateInvalid();
+      try
+      {
+        var result = Mapper.Map<DonationSubType>(data);
+        await UnitOfWork.DonationSubTypes.Insert(result);
+        await UnitOfWork.Save();
+        return Ok(result);
+      }
+      catch (Exception ex)
+      {
+        return CatchException(ex, nameof(Create));
+      }
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<IActionResult> Update(int id, [FromBody] DonationSubTypeDtoCreate data)
+    {
+      if (!ModelState.IsValid || id < 1) return UpdateInvalid();
+      try
+      {
+        var item = await UnitOfWork.DonationSubTypes.Get(q => q.Id == id);
+
+        if (item == null) return UpdateNull();
+
+        var result = Mapper.Map(data, item);
+        UnitOfWork.DonationSubTypes.Update(item);
+        await UnitOfWork.Save();
+        return Ok(result);
+      }
+      catch (Exception ex)
+      {
+        return CatchException(ex, nameof(Update));
+      }
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+      if (id < 1) return DeleteInvalid();
+
+      var search = await UnitOfWork.DonationSubTypes.Get(q => q.Id == id);
+      if (search == null) return DeleteNull();
+
+      try
+      {
+        await UnitOfWork.DonationSubTypes.Delete(id);
+        await UnitOfWork.Save();
+      }
+      catch (Exception ex)
+      {
+        return CatchException(ex, nameof(Delete));
+      }
+      return NoContent();
+    }
   }
 }
