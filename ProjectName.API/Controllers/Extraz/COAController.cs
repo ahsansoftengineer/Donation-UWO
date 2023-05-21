28cee7f389bc67694640501b6653c0142b31c06d
Muhammad Ahsan <53137713+ahsansoftengineer@users.noreply.github.com>
2023-05-21 18:15:07 +0500

    Common Controller Working

--- ProjectName.API/Controllers/Extraz/COAController.cs
+++ ProjectName.API/Controllers/Extraz/COAController.cs
@@ -1,11 +1,106 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using ProjectName.API.Controllers.Base;
+using ProjectName.Domain.Base;
+using ProjectName.Domain.Common;
+using ProjectName.Infra.Entity.Extraz;
+using ProjectName.Infra.Repo;
+using X.PagedList;
 
 namespace ProjectName.API.Controllers.Extraz
 {
   [Route("api/[controller]")]
   [ApiController]
-  public class COAController : ControllerBase
+  public class COAController : BaseController<COAController>
   {
+    public COAController(
+      ILogger<COAController> logger,
+      IMapper mapper,
+      IUnitOfWork unitOfWork) : base(logger, mapper, unitOfWork)
+    { }
+
+    [HttpGet]
+    public async Task<IActionResult> Gets([FromQuery] PaginateRequestFilter<COA, CommonDtoSearch?> filter)
+    {
+      try
+      {
+        var list = await UnitOfWork.COAs.Gets(filter);
+        var result = Mapper.Map<IPagedList<COA>, PaginateResponse<CommonDto>>(list);
+        return Ok(result);
+      }
+      catch (Exception ex)
+      {
+        return CatchException(ex, nameof(Gets));
+      }
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> Get(int id)
+    {
+      var single = await UnitOfWork.COAs.Get(
+        q => q.Id == id
+     //, new List<string> { "Org" }
+     );
+      var result = Mapper.Map<BaseDtoSingle<CommonDto>>(single);
+      return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CommonDtoCreate data)
+    {
+      if (!ModelState.IsValid) return CreateInvalid();
+      try
+      {
+        var result = Mapper.Map<COA>(data);
+        await UnitOfWork.COAs.Insert(result);
+        await UnitOfWork.Save();
+        return Ok(result);
+      }
+      catch (Exception ex)
+      {
+        return CatchException(ex, nameof(Create));
+      }
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<IActionResult> Update(int id, [FromBody] CommonDtoCreate data)
+    {
+      if (!ModelState.IsValid || id < 1) return UpdateInvalid();
+      try
+      {
+        var item = await UnitOfWork.COAs.Get(q => q.Id == id);
+
+        if (item == null) return UpdateNull();
+
+        var result = Mapper.Map(data, item);
+        UnitOfWork.COAs.Update(item);
+        await UnitOfWork.Save();
+        return Ok(result);
+      }
+      catch (Exception ex)
+      {
+        return CatchException(ex, nameof(Update));
+      }
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+      if (id < 1) return DeleteInvalid();
+
+      var search = await UnitOfWork.COAs.Get(q => q.Id == id);
+      if (search == null) return DeleteNull();
+
+      try
+      {
+        await UnitOfWork.COAs.Delete(id);
+        await UnitOfWork.Save();
+      }
+      catch (Exception ex)
+      {
+        return CatchException(ex, nameof(Delete));
+      }
+      return NoContent();
+    }
   }
 }
--- ProjectName.API/Controllers/Extraz/SysmanAccountController.cs
+++ ProjectName.API/Controllers/Extraz/SysmanAccountController.cs
@@ -1,10 +1,106 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using ProjectName.API.Controllers.Base;
+using ProjectName.Domain.Base;
+using ProjectName.Domain.Common;
+using ProjectName.Infra.Entity.Extraz;
+using ProjectName.Infra.Repo;
+using X.PagedList;
 
 namespace ProjectName.API.Controllers.Extraz
 {
   [Route("api/[controller]")]
   [ApiController]
-  public class SysmanAccountController : ControllerBase
+  public class SysmanAccountController : BaseController<SysmanAccountController>
   {
+    public SysmanAccountController(
+      ILogger<SysmanAccountController> logger,
+      IMapper mapper,
+      IUnitOfWork unitOfWork) : base(logger, mapper, unitOfWork)
+    { }
+
+    [HttpGet]
+    public async Task<IActionResult> Gets([FromQuery] PaginateRequestFilter<SysmanAccount, CommonDtoSearch?> filter)
+    {
+      try
+      {
+        var list = await UnitOfWork.SysmanAccounts.Gets(filter);
+        var result = Mapper.Map<IPagedList<SysmanAccount>, PaginateResponse<CommonDto>>(list);
+        return Ok(result);
+      }
+      catch (Exception ex)
+      {
+        return CatchException(ex, nameof(Gets));
+      }
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> Get(int id)
+    {
+      var single = await UnitOfWork.SysmanAccounts.Get(
+        q => q.Id == id
+     //, new List<string> { "Org" }
+     );
+      var result = Mapper.Map<BaseDtoSingle<CommonDto>>(single);
+      return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CommonDtoCreate data)
+    {
+      if (!ModelState.IsValid) return CreateInvalid();
+      try
+      {
+        var result = Mapper.Map<SysmanAccount>(data);
+        await UnitOfWork.SysmanAccounts.Insert(result);
+        await UnitOfWork.Save();
+        return Ok(result);
+      }
+      catch (Exception ex)
+      {
+        return CatchException(ex, nameof(Create));
+      }
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<IActionResult> Update(int id, [FromBody] CommonDtoCreate data)
+    {
+      if (!ModelState.IsValid || id < 1) return UpdateInvalid();
+      try
+      {
+        var item = await UnitOfWork.SysmanAccounts.Get(q => q.Id == id);
+
+        if (item == null) return UpdateNull();
+
+        var result = Mapper.Map(data, item);
+        UnitOfWork.SysmanAccounts.Update(item);
+        await UnitOfWork.Save();
+        return Ok(result);
+      }
+      catch (Exception ex)
+      {
+        return CatchException(ex, nameof(Update));
+      }
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+      if (id < 1) return DeleteInvalid();
+
+      var search = await UnitOfWork.SysmanAccounts.Get(q => q.Id == id);
+      if (search == null) return DeleteNull();
+
+      try
+      {
+        await UnitOfWork.SysmanAccounts.Delete(id);
+        await UnitOfWork.Save();
+      }
+      catch (Exception ex)
+      {
+        return CatchException(ex, nameof(Delete));
+      }
+      return NoContent();
+    }
   }
 }
